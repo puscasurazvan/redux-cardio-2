ea8c18b8ab5b2794f5983395fa7c0041c1e1d50a
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-02-08 21:25:43 +0000

    connect the toggle

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import React from 'react'
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
+import logo from './logo.svg'
+import './App.css'
+
+import MoviesList from './components/MoviesList'
+import MovieDetail from './components/MovieDetail'
+import Toggle from './components/Toggle'
+
+import rootReducer from './redux/rootReducer'
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(),
   )
-}
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+        </header>
+        <Toggle />
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
+)
 
 export default App
-
-const defaultState = {
-  welcome: 'Hello',
-  otherState: 'some stuff'
-}
-
-const greeting = (state = defaultState, action) => {
-  switch (action.type) {
-    case 'GREET_ME':
-      return { welcome: 'Hello Raz'}
-    case 'GREET_WORLD':
-      return { welcome: 'Hello World'}
-    default:
-      return state
-  }
-}
-
-const store = createStore(greeting)
-
-console.log(store.getState())
---
+++ src/components/Toggle.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+const Toggle = ({ messageVisibility }) => (
+  <div>
+    {messageVisibility && 
+      <p>You will be seeing this if redux action is toggled</p>
+    }
+    <button>Toggle me</button>
+  </div>
+)
+
+const mapStateToProps = state => ({
+  messageVisibility: state.message.messageVisibility
+})
+
+export default connect(mapStateToProps)(Toggle)

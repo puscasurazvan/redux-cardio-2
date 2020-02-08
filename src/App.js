d7ac1e46b697782b736ed007241b4ade4697fd18
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-02-08 20:09:29 +0000

    hello discord

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from 'react'
+
+import { createStore } from 'redux'
 
 function App() {
   return (
     <div className="App">
       Hi
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+const defaultState = {
+  welcome: 'Hello',
+  otherState: 'some stuff'
+}
+
+const greeting = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'GREET_ME':
+      return { welcome: 'Hello Raz'}
+    case 'GREET_WORLD':
+      return { welcome: 'Hello World'}
+    default:
+      return state
+  }
+}
+
+const store = createStore(greeting)
+
+console.log(store.getState())
